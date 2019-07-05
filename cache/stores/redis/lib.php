d698569b3769e78a589461fbc08037b3573e8fd5
ewallah <info@ewallah.net>
2019-07-05 10:16:02 +0200

    undo MDL-65941

--- cache/stores/redis/lib.php
+++ cache/stores/redis/lib.php
@@ -156,21 +156,17 @@ class cachestore_redis extends cache_store implements cache_is_key_aware, cache_
             $server = $serverconf[0];
             $port = $serverconf[1];
         }
-        try {
-            if ($redis->connect($server, $port)) {
-                if (!empty($password)) {
-                    $redis->auth($password);
-                }
-                $redis->setOption(Redis::OPT_SERIALIZER, $this->serializer);
-                if (!empty($prefix)) {
-                    $redis->setOption(Redis::OPT_PREFIX, $prefix);
-                }
-                // Database setting option...
-                $this->isready = $this->ping($redis);
-            } else {
-                $this->isready = false;
+        if ($redis->connect($server, $port)) {
+            if (!empty($password)) {
+                $redis->auth($password);
             }
-        } catch (\RedisException $e) {
+            $redis->setOption(Redis::OPT_SERIALIZER, $this->serializer);
+            if (!empty($prefix)) {
+                $redis->setOption(Redis::OPT_PREFIX, $prefix);
+            }
+            // Database setting option...
+            $this->isready = $this->ping($redis);
+        } else {
             $this->isready = false;
         }
         return $redis;
